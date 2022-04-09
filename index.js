562855d456c9007e5d3f55fd6eaceff678416854
Brindia Joseph <brindiatyjuana@gmail.com>
2022-04-09 10:04:22 -0500

    commit tree df26a39d4329d880e7f8c9fd72523cec72bfa690

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"] 
+// this is an array
+
+function destructivelyAppendCat (name) {
+    cats.push(name);
+  } // push array method
+  
+  function destructivelyPrependCat (name) {
+    cats.unshift(name);
+  } //unshift methos// 
+  
+  function destructivelyRemoveLastCat () {
+    cats.pop();
+  }// pop method
+  
+  function destructivelyRemoveFirstCat () {
+    cats.shift();
+  }// shift method
+  
+  function appendCat (name) {
+    return [...cats, name];
+  }// spread opperater 
+  
+  function prependCat (name) {
+    return [name, ...cats];
+  }// spread operator
+  
+  function removeFirstCat () {
+    return cats.slice(1);
+  }// slice method
+  
+  function removeLastCat () {
+    return cats.slice(0, cats.length - 1);
+  }//slice metod
